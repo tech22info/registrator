34d7eb460440f765a4477535ea06c1aeeafb2885
anton@th22.info <root@ns1.internal.th22.info>
2015-05-29 10:47:56 +0600

    DB Schema updated

--- db_structure/schema.sql
+++ db_structure/schema.sql
@@ -2,9 +2,9 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.3.6
+-- Dumped from database version 9.3.7
 -- Dumped by pg_dump version 9.4.1
--- Started on 2015-05-26 11:43:34 NOVT
+-- Started on 2015-05-29 10:45:41 NOVT
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -14,7 +14,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 
 --
--- TOC entry 177 (class 3079 OID 11759)
+-- TOC entry 179 (class 3079 OID 11759)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
@@ -22,8 +22,8 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 1982 (class 0 OID 0)
--- Dependencies: 177
+-- TOC entry 2002 (class 0 OID 0)
+-- Dependencies: 179
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -48,11 +48,23 @@ CREATE TABLE email_mailru (
     password character varying(60),
     fio character varying(240),
     birthday character varying(60),
-    male boolean
+    male boolean,
+    created timestamp without time zone DEFAULT now(),
+    active boolean DEFAULT false,
+    error_counter integer DEFAULT 0
 );
 
 
 ALTER TABLE email_mailru OWNER TO web_script;
+
+--
+-- TOC entry 2003 (class 0 OID 0)
+-- Dependencies: 170
+-- Name: TABLE email_mailru; Type: COMMENT; Schema: public; Owner: web_script
+--
+
+COMMENT ON TABLE email_mailru IS 'Проект "Регистратор". Таблица аккаунтов на Mail.ru';
+
 
 --
 -- TOC entry 171 (class 1259 OID 19349)
@@ -70,7 +82,7 @@ CREATE SEQUENCE email_mailru_id_seq
 ALTER TABLE email_mailru_id_seq OWNER TO web_script;
 
 --
--- TOC entry 1983 (class 0 OID 0)
+-- TOC entry 2004 (class 0 OID 0)
 -- Dependencies: 171
 -- Name: email_mailru_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: web_script
 --
@@ -92,6 +104,15 @@ CREATE TABLE family_name (
 ALTER TABLE family_name OWNER TO web_script;
 
 --
+-- TOC entry 2005 (class 0 OID 0)
+-- Dependencies: 172
+-- Name: TABLE family_name; Type: COMMENT; Schema: public; Owner: web_script
+--
+
+COMMENT ON TABLE family_name IS 'Проект "Виртуальные-люди". Таблица фамилий.';
+
+
+--
 -- TOC entry 173 (class 1259 OID 19366)
 -- Name: first_name; Type: TABLE; Schema: public; Owner: web_script; Tablespace: 
 --
@@ -103,6 +124,38 @@ CREATE TABLE first_name (
 
 
 ALTER TABLE first_name OWNER TO web_script;
+
+--
+-- TOC entry 2006 (class 0 OID 0)
+-- Dependencies: 173
+-- Name: TABLE first_name; Type: COMMENT; Schema: public; Owner: web_script
+--
+
+COMMENT ON TABLE first_name IS 'Проект "Виртуальные-люди". Таблица имен.';
+
+
+--
+-- TOC entry 177 (class 1259 OID 19417)
+-- Name: fotostrana_accounts; Type: TABLE; Schema: public; Owner: web_script; Tablespace: 
+--
+
+CREATE TABLE fotostrana_accounts (
+    email_id integer NOT NULL,
+    email_send boolean DEFAULT false,
+    email_activated boolean DEFAULT false
+);
+
+
+ALTER TABLE fotostrana_accounts OWNER TO web_script;
+
+--
+-- TOC entry 2007 (class 0 OID 0)
+-- Dependencies: 177
+-- Name: TABLE fotostrana_accounts; Type: COMMENT; Schema: public; Owner: web_script
+--
+
+COMMENT ON TABLE fotostrana_accounts IS 'Проект "Регистратор". Таблица аккаунтов на Фотостране (Привязка к Email)';
+
 
 --
 -- TOC entry 175 (class 1259 OID 19376)
@@ -121,6 +174,15 @@ CREATE TABLE https_proxy (
 ALTER TABLE https_proxy OWNER TO web_script;
 
 --
+-- TOC entry 2008 (class 0 OID 0)
+-- Dependencies: 175
+-- Name: TABLE https_proxy; Type: COMMENT; Schema: public; Owner: web_script
+--
+
+COMMENT ON TABLE https_proxy IS 'Проект "Прокси". Таблица прокси-серверов с поддержкой HTTPS';
+
+
+--
 -- TOC entry 174 (class 1259 OID 19371)
 -- Name: last_name; Type: TABLE; Schema: public; Owner: web_script; Tablespace: 
 --
@@ -132,6 +194,41 @@ CREATE TABLE last_name (
 
 
 ALTER TABLE last_name OWNER TO web_script;
+
+--
+-- TOC entry 2009 (class 0 OID 0)
+-- Dependencies: 174
+-- Name: TABLE last_name; Type: COMMENT; Schema: public; Owner: web_script
+--
+
+COMMENT ON TABLE last_name IS 'Проект "Виртуальные-люди". Таблица отчеств.';
+
+
+--
+-- TOC entry 178 (class 1259 OID 19426)
+-- Name: livejournal_accounts; Type: TABLE; Schema: public; Owner: web_script; Tablespace: 
+--
+
+CREATE TABLE livejournal_accounts (
+    email_id integer NOT NULL,
+    email_send boolean DEFAULT false,
+    email_activated boolean DEFAULT false,
+    error_counter integer DEFAULT 0,
+    password character varying(120),
+    login character varying(120)
+);
+
+
+ALTER TABLE livejournal_accounts OWNER TO web_script;
+
+--
+-- TOC entry 2010 (class 0 OID 0)
+-- Dependencies: 178
+-- Name: TABLE livejournal_accounts; Type: COMMENT; Schema: public; Owner: web_script
+--
+
+COMMENT ON TABLE livejournal_accounts IS 'Проект "Регистратор". Таблица аккаунтов на LiveJournal (Привязка к Email)';
+
 
 --
 -- TOC entry 176 (class 1259 OID 19384)
@@ -150,7 +247,7 @@ CREATE VIEW random_https_proxy AS
 ALTER TABLE random_https_proxy OWNER TO web_script;
 
 --
--- TOC entry 1852 (class 2604 OID 19351)
+-- TOC entry 1860 (class 2604 OID 19351)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: web_script
 --
 
@@ -158,7 +255,7 @@ ALTER TABLE ONLY email_mailru ALTER COLUMN id SET DEFAULT nextval('email_mailru_
 
 
 --
--- TOC entry 1858 (class 2606 OID 19359)
+-- TOC entry 1874 (class 2606 OID 19359)
 -- Name: email_mailru_pkey; Type: CONSTRAINT; Schema: public; Owner: web_script; Tablespace: 
 --
 
@@ -167,7 +264,7 @@ ALTER TABLE ONLY email_mailru
 
 
 --
--- TOC entry 1860 (class 2606 OID 19365)
+-- TOC entry 1876 (class 2606 OID 19365)
 -- Name: family_name_pkey; Type: CONSTRAINT; Schema: public; Owner: web_script; Tablespace: 
 --
 
@@ -176,7 +273,7 @@ ALTER TABLE ONLY family_name
 
 
 --
--- TOC entry 1862 (class 2606 OID 19370)
+-- TOC entry 1878 (class 2606 OID 19370)
 -- Name: first_name_pkey; Type: CONSTRAINT; Schema: public; Owner: web_script; Tablespace: 
 --
 
@@ -185,7 +282,16 @@ ALTER TABLE ONLY first_name
 
 
 --
--- TOC entry 1866 (class 2606 OID 19383)
+-- TOC entry 1884 (class 2606 OID 19421)
+-- Name: fotostrana_accounts_pkey; Type: CONSTRAINT; Schema: public; Owner: web_script; Tablespace: 
+--
+
+ALTER TABLE ONLY fotostrana_accounts
+    ADD CONSTRAINT fotostrana_accounts_pkey PRIMARY KEY (email_id);
+
+
+--
+-- TOC entry 1882 (class 2606 OID 19383)
 -- Name: https_pkey; Type: CONSTRAINT; Schema: public; Owner: web_script; Tablespace: 
 --
 
@@ -194,7 +300,7 @@ ALTER TABLE ONLY https_proxy
 
 
 --
--- TOC entry 1864 (class 2606 OID 19375)
+-- TOC entry 1880 (class 2606 OID 19375)
 -- Name: last_name_pkey; Type: CONSTRAINT; Schema: public; Owner: web_script; Tablespace: 
 --
 
@@ -203,7 +309,16 @@ ALTER TABLE ONLY last_name
 
 
 --
--- TOC entry 1856 (class 1259 OID 19360)
+-- TOC entry 1886 (class 2606 OID 19430)
+-- Name: livejournal_accounts_pkey; Type: CONSTRAINT; Schema: public; Owner: web_script; Tablespace: 
+--
+
+ALTER TABLE ONLY livejournal_accounts
+    ADD CONSTRAINT livejournal_accounts_pkey PRIMARY KEY (email_id);
+
+
+--
+-- TOC entry 1872 (class 1259 OID 19360)
 -- Name: email_mailru_email_domain_idx; Type: INDEX; Schema: public; Owner: web_script; Tablespace: 
 --
 
@@ -211,7 +326,7 @@ CREATE UNIQUE INDEX email_mailru_email_domain_idx ON email_mailru USING btree (e
 
 
 --
--- TOC entry 1981 (class 0 OID 0)
+-- TOC entry 2001 (class 0 OID 0)
 -- Dependencies: 6
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
@@ -222,7 +337,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2015-05-26 11:44:05 NOVT
+-- Completed on 2015-05-29 10:46:14 NOVT
 
 --
 -- PostgreSQL database dump complete
